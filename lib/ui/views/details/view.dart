899d35108f7974525490301fb78739d9ecffb485
Priscilla <31478774+Nicole-ABC@users.noreply.github.com>
2022-11-15 09:02:19 +0100

    add api data

---
+++ lib/ui/views/details/view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsView extends StatefulWidget {
+  DetailsView({Key? key}) : super(key: key);
+
+  @override
+  State<DetailsView> createState() => _DetailsViewState();
+}
+
+class _DetailsViewState extends State<DetailsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
